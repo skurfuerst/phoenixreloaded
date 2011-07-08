9f3ab46d63cf784f1f576a25a44433fb2726deab
Christopher Hlubek <hlubek@networkteam.com>
2011-07-08 15:02:35 +0200

    Implemented toolbar with preview button

--- js/app.js
+++ js/app.js
@@ -5,33 +5,63 @@ ContentModule = SC.Application.create({
 		this._initializeToolbar();
 		this._initializeFooter();
 
-		$('body').addClass('typo3-backend');
+		$('body').addClass('t3-backend');
 	},
 
 	_initializePropertyPanel: function() {
-		$('body').append($('<div class="typo3-rightarea aloha-block-do-not-deactivate" id="t3-ui-rightarea" />')); // TODO: change prefix to "t3"; change style names to "Property Panel"
+		$('body').append($('<div class="t3-rightarea aloha-block-do-not-deactivate" id="t3-ui-rightarea" />')); // TODO: change prefix to "t3"; change style names to "Property Panel"
 
 		var propertyPanelView = SC.View.create({
 			template: SC.Handlebars.compile('{{#collection contentBinding="ContentModule.CurrentlyActivatedBlockSchema"}}\
 {{view ContentModule.PropertyPanelSection}}\
  {{/collection}}!')
 		});
-		propertyPanelView.appendTo($('.typo3-rightarea'));
+		propertyPanelView.appendTo($('.t3-rightarea'));
 	},
+
 	_initializeToolbar: function() {
-		$('body').append($('<div class="typo3-actionmenu aloha-block-do-not-deactivate" id="t3-ui-actionmenu" />')); // TODO. rename CSS to toolbar
+		var button = SC.Button.extend({
+			target: 'ContentModule.PreviewController',
+			action: 'togglePreview',
+			content: 'Foo',
+			classNames: 't3-button',
+			template: SC.Handlebars.compile('Preview')
+		});
+		var toolbar = ContentModule.Toolbar.create({
+			left: [
+				button
+			]
+		});
+		toolbar.appendTo($('body'));
+
 	},
 	_initializeFooter: function() {
-		$('body').append($('<div class="typo3-breadcrumbmenu aloha-block-do-not-deactivate" id="t3-ui-breadcrumbmenu" />')); // TODO: rename CSS
+		$('body').append($('<div class="t3-breadcrumbmenu aloha-block-do-not-deactivate" id="t3-ui-breadcrumbmenu" />')); // TODO: rename CSS
 
 		var breadcrumbMenuView = SC.View.create({
 			template: SC.Handlebars.compile('{{#collection contentBinding="ContentModule.BlockSelectionController"}} {{content.title}} {{/collection}}!')
 		});
-		breadcrumbMenuView.appendTo($('.typo3-breadcrumbmenu'));
+		breadcrumbMenuView.appendTo($('.t3-breadcrumbmenu'));
 	},
 
 	_onBlockSelectionChange: function(selectedBlocks) {
 		ContentModule.BlockSelectionController.updateSelectedBlocks(selectedBlocks);
+	}
+
+});
+
+ContentModule.Toolbar = SC.View.extend({
+	tagName: 'div',
+	classNames: ['t3-toolbar aloha-block-do-not-deactivate'],
+	template: SC.Handlebars.compile('{{#collection contentBinding="parentView.left" tagName="ul" classNames="t3-toolbar-left"}}{{view content}}{{/collection}}{{#collection contentBinding="parentView.right" tagName="ul" classNames="t3-toolbar-right"}}{{view content}}{{/collection}}')
+});
+
+ContentModule.PreviewController = SC.Object.create({
+	previewMode: false,
+
+	togglePreview: function() {
+		console.log('Toogle preview');
+		this.set('previewMode', !this.get('previewMode'));
 	}
 });
 
